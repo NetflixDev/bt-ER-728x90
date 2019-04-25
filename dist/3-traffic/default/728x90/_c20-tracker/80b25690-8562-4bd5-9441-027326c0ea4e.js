4fd5f94faf39c5738f9bdb55f35e3afbb09e21c4
Derek Miranda <derek.miranda@ff0000.com>
2019-04-25 15:49:39 -0700

    fix: committing previously ignored files
    - due to BSA .gitignore

---
+++ dist/3-traffic/default/728x90/_c20-tracker/80b25690-8562-4bd5-9441-027326c0ea4e.js
@@ -0,0 +1,23 @@
+// GENERIC SOURCE TRACKER: bt-Endframe-Resolve
+if (typeof module === 'undefined') {
+	module = {}
+}
+// prettier-ignore
+module.exports = {
+  "id": "80b25690-8562-4bd5-9441-027326c0ea4e",
+  "name": "bt-ER-728x90-stacked",
+  "label": "Stacked",
+  "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
+  "type": "build_templates",
+  "tier": "endframe-resolve",
+  "width": "728",
+  "height": "90",
+  "creative": "./3-traffic/default/728x90",
+  "context": "RED Interactive",
+  "state": "published",
+  "updated": 1555446786324,
+  "username": "GitHub",
+  "full_name": "NetflixDev/bt-ER-728x90-stacked",
+  "html_url": "https://github.com/NetflixDev/bt-ER-728x90-stacked",
+  "version": "2.2.1"
+};
