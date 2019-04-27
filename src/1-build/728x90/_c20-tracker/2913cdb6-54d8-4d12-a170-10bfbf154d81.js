199a3d8cbcf2e1a2669f2002db9e020cd96e9983
Derek Miranda <derek.miranda@ff0000.com>
2019-04-26 17:11:22 -0700

    pkg: redist, repub

--- src/1-build/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
+++ src/1-build/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
@@ -12,12 +12,19 @@ module.exports = {
     "label": "728x90 NF Global Template",
     "tier": "endframe-resolve",
     "size": "728x90",
-    "creative": "./dist/3-traffic/default/728x90",
-    "toolkit": "./toolkit/300x600_NF_Global_Template_ContainerRecycle_Layered.psd"
+    "creative": "./3-traffic/default/728x90",
+    "toolkit": "./toolkit/300x600_NF_Global_Template_ContainerRecycle_Layered.psd",
+    "layouts": {
+      "SIDE_BY_SIDE_LEFT": "Side-By-Side Left",
+      "SIDE_BY_SIDE_WIDE": "Side-By-Side Wide",
+      "STACKED_LEFT": "Stacked Left",
+      "STACKED_CENTER": "Stacked Center",
+      "STACKED_WIDE": "Stacked Wide"
+    }
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1556238594250,
+  "updated": 1556323818004,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-728x90",
   "html_url": "https://github.com/NetflixDev/bt-ER-728x90",
