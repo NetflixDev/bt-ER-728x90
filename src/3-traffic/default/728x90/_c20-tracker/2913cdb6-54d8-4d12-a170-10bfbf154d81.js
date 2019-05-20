942ea2977c6219efc9dc529ab93e66def4889189
Derek Miranda <derek.miranda@ff0000.com>
2019-05-20 12:47:08 -0700

    feature: puts supercut toggle back
     - fix: fixes CTA over/out gestures
     - redist

--- src/3-traffic/default/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
+++ src/3-traffic/default/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557970140231,
+  "updated": 1558381614110,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-728x90",
   "html_url": "https://github.com/NetflixDev/bt-ER-728x90",
