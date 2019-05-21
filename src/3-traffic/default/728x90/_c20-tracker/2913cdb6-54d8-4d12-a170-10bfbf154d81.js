a61b5ff9d7cb12f53db8b9c35dd924f6cc071ce5
Greg Connell <greg.connell@ff0000.com>
2019-05-21 16:13:55 -0700

    moves cta

--- src/3-traffic/default/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
+++ src/3-traffic/default/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1558475551614,
+  "updated": 1558480420903,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-728x90",
   "html_url": "https://github.com/NetflixDev/bt-ER-728x90",
