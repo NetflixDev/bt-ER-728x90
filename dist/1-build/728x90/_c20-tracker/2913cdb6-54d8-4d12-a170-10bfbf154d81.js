09e017aef1252c54f654fca5653f6ca807079d4d
Derek Miranda <derek.miranda@ff0000.com>
2019-07-17 15:23:57 -0700

    fix: constrains CTA width to logo
    - for layouts where CTA and logo vertically laid out
    - redist + pub

--- dist/1-build/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
+++ dist/1-build/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1562188894504,
+  "updated": 1563402209048,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-728x90",
   "html_url": "https://github.com/NetflixDev/bt-ER-728x90",
