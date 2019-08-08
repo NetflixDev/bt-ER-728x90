66fde5e6c566393acd1a97c629e384dd0f5b3398
Derek Miranda <derek.miranda@ff0000.com>
2019-08-08 12:25:14 -0700

    npm: C2.0 BT redist

--- dist/3-traffic/default/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
+++ dist/3-traffic/default/728x90/_c20-tracker/2913cdb6-54d8-4d12-a170-10bfbf154d81.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1565129088607,
+  "updated": 1565292275548,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-728x90",
   "html_url": "https://github.com/NetflixDev/bt-ER-728x90",
