2d832a84329a0ae4c5e6548450a0f6bce77cd144
Derek Miranda <derek.miranda@ff0000.com>
2019-09-09 21:39:29 -0700

    Mobile intro zoom fix

--- src/1-build/common/js/EndFrame/postmarkups/sideBySidePostMarkup.js
+++ src/1-build/common/js/EndFrame/postmarkups/sideBySidePostMarkup.js
@@ -1,128 +1,119 @@
-import "@netflixadseng/wc-netflix-brand-logo";
-import "@netflixadseng/wc-netflix-cta";
-import "@netflixadseng/wc-netflix-text";
-import "@netflixadseng/wc-netflix-img";
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { ImageManager } from "ad-control";
-import { Animation } from "@common/js/Animation.js";
-import { Control } from "@common/js/Control.js";
-import "@netflixadseng/wc-netflix-flushed-ribbon";
-import CanvasIris from "@common/js/CanvasIris.js";
-import {
-  UIComponent,
-  UIBorder,
-  UIButton,
-  UIImage,
-  TextFormat,
-  UITextField,
-  UISvg,
-  UIGroup
-} from "ad-ui";
-import { ObjectUtils } from "ad-utils";
-import { titleTreatmentLayout } from "./shared.js";
+import '@netflixadseng/wc-netflix-brand-logo'
+import '@netflixadseng/wc-netflix-cta'
+import '@netflixadseng/wc-netflix-text'
+import '@netflixadseng/wc-netflix-img'
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import '@netflixadseng/wc-netflix-flushed-ribbon'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg, UIGroup } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+import { titleTreatmentLayout } from './shared.js'
 
 export default function sideBySidePostMarkup() {
-  let T = View.endFrame;
+	let T = View.endFrame
 
-  // title treatment
-  titleTreatmentLayout(T);
+	// title treatment
+	titleTreatmentLayout(T)
 
-  Align.set(T.pedigree, {
-    x: {
-      type: Align.CENTER,
-      against: T.tt
-    },
-    y: {
-      type: Align.CENTER,
-      against: 65
-    }
-  });
+	Align.set(T.pedigree, {
+		x: {
+			type: Align.CENTER,
+			against: T.tt
+		},
+		y: {
+			type: Align.CENTER,
+			against: 65
+		}
+	})
 
-  // cta
-  var logoCtaY = adData.hasFTM || adData.hasTuneIn ? 45 : 32;
-  T.cta.resize();
-  Align.set(T.cta, {
-    x: {
-      type: Align.RIGHT,
-      offset: -20
-    },
-    y: {
-      type: Align.TOP,
-      offset: logoCtaY
-    }
-  });
+	// cta
+	var logoCtaY = adData.hasFTM || adData.hasTuneIn ? 45 : 32
+	T.cta.resize()
+	Align.set(T.cta, {
+		x: {
+			type: Align.RIGHT,
+			offset: -20
+		},
+		y: {
+			type: Align.TOP,
+			offset: logoCtaY
+		}
+	})
 
-  // logo
-  Align.set(T.netflixLogo, {
-    x: {
-      type: Align.LEFT,
-      outer: true,
-      against: T.cta,
-      offset: -14
-    },
-    y: {
-      type: Align.TOP,
-      offset: logoCtaY
-    }
-  });
+	// logo
+	Align.set(T.netflixLogo, {
+		x: {
+			type: Align.LEFT,
+			outer: true,
+			against: T.cta,
+			offset: -14
+		},
+		y: {
+			type: Align.TOP,
+			offset: logoCtaY
+		}
+	})
 
-  // lockup to position CTA and logo together
-  T.brandingLockup = new UIGroup({
-    target: T,
-    children: [T.cta, T.netflixLogo]
-  });
+	// lockup to position CTA and logo together
+	T.brandingLockup = new UIGroup({
+		target: T,
+		children: [T.cta, T.netflixLogo]
+	})
 
-  if (adData.hasFTM) {
-    // free trial messaging
-    Styles.setCss(T.ftm, {
-      color: "#fff",
-      fontSize: 14,
-      letterSpacing: 1,
-      textAlign: "center"
-    });
-    Align.set(T.ftm, {
-      x: { type: Align.CENTER, against: T.brandingLockup },
-      y: {
-        type: Align.TOP,
-        outer: true,
-        against: T.brandingLockup,
-        offset: -8
-      }
-    });
-    T.removeChild(T.tuneIn);
-  } else {
-    // tune-in
-    Styles.setCss(T.tuneIn, {
-      color: "#fff",
-      fontSize: 16,
-      letterSpacing: 1,
-      textAlign: "center"
-    });
-    Align.set(T.tuneIn, {
-      x: { type: Align.CENTER, against: T.brandingLockup },
-      y: {
-        type: Align.TOP,
-        outer: true,
-        against: T.brandingLockup,
-        offset: -8
-      }
-    });
-    T.removeChild(T.ftm);
-  }
+	if (adData.hasFTM) {
+		// free trial messaging
+		Styles.setCss(T.ftm, {
+			color: '#fff',
+			fontSize: 14,
+			letterSpacing: 1,
+			textAlign: 'center'
+		})
+		Align.set(T.ftm, {
+			x: { type: Align.CENTER, against: T.brandingLockup },
+			y: {
+				type: Align.TOP,
+				outer: true,
+				against: T.brandingLockup,
+				offset: -8
+			}
+		})
+		T.removeChild(T.tuneIn)
+	} else {
+		// tune-in
+		Styles.setCss(T.tuneIn, {
+			color: '#fff',
+			fontSize: 16,
+			letterSpacing: 1,
+			textAlign: 'center'
+		})
+		Align.set(T.tuneIn, {
+			x: { type: Align.CENTER, against: T.brandingLockup },
+			y: {
+				type: Align.TOP,
+				outer: true,
+				against: T.brandingLockup,
+				offset: -8
+			}
+		})
+		T.removeChild(T.ftm)
+	}
 
-  // ratings bug
-  if (adData.hasRatings) {
-    Align.set(T.ratingsBug, {
-      x: {
-        type: Align.RIGHT,
-        offset: -5
-      },
-      y: {
-        type: Align.BOTTOM,
-        offset: -5
-      }
-    });
-  } else {
-    T.removeChild(T.ratingsBug);
-  }
+	// ratings bug
+	if (adData.hasRatings) {
+		Align.set(T.ratingsBug, {
+			x: {
+				type: Align.RIGHT,
+				offset: -5
+			},
+			y: {
+				type: Align.BOTTOM,
+				offset: -5
+			}
+		})
+	} else {
+		T.removeChild(T.ratingsBug)
+	}
 }
