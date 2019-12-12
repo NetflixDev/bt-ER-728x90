eade90fdb7c09a09e648788d70fdb8e26b04dce9
Derek Miranda <derek.miranda@ff0000.com>
2019-12-12 13:39:41 -0800

    Applies FAT refactor changes
    - redist

--- src/1-build/common/js/EndFrame/inits/index.js
+++ src/1-build/common/js/EndFrame/inits/index.js
@@ -1,39 +1,26 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { ImageManager } from "ad-control";
-import { Animation } from "@common/js/Animation.js";
-import { Control } from "@common/js/Control.js";
-import CanvasIris from "@common/js/CanvasIris.js";
-import {
-  UIComponent,
-  UIBorder,
-  UIButton,
-  UIImage,
-  TextFormat,
-  UITextField,
-  UISvg
-} from "ad-ui";
-import { ObjectUtils } from "ad-utils";
-import baseInit from "./baseInit.js";
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+import baseInit from './baseInit.js'
 
-export { sideBySideInit, stackedInit };
+export { sideBySideInit, stackedInit }
 
 function sideBySideInit(T) {
-  baseInit(T, {
-    logoWidth: 112,
-    ctaWidth: 107,
-    ctaMaxWidth: 130,
-    ctaHeight: 30
-  });
+	baseInit(T, { logoWidth: 112, ctaWidth: 107, ctaMaxWidth: 130, ctaHeight: 30 })
 }
 
 function stackedInit(T) {
-  baseInit(T, {
-    logoWidth: 76,
-    ctaWidth: 76,
-    ctaHeight: 19,
-    extraCtaLogoInit: cta => {
-      cta.setAttribute("min-font-size", 7);
-      cta.setAttribute("horizontal-pad", "9%");
-    }
-  });
+	baseInit(T, {
+		logoWidth: 76,
+		ctaWidth: 76,
+		ctaHeight: 19,
+		extraCtaLogoInit: cta => {
+			cta.setAttribute('min-font-size', 7)
+			cta.setAttribute('horizontal-pad', '9%')
+		}
+	})
 }
--- src/1-build/common/js/EndFrame/postmarkups/stackedPostMarkup.js
+++ src/1-build/common/js/EndFrame/postmarkups/stackedPostMarkup.js
@@ -1,63 +1,55 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { ImageManager } from "ad-control";
-import { Animation } from "@common/js/Animation.js";
-import { Control } from "@common/js/Control.js";
-import CanvasIris from "@common/js/CanvasIris.js";
-import {
-  UIComponent,
-  UIBorder,
-  UIButton,
-  UIImage,
-  TextFormat,
-  UITextField,
-  UISvg
-} from "ad-ui";
-import { ObjectUtils } from "ad-utils";
-import { titleTreatmentLayout, stackedBrandingLockup } from "./shared.js";
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+import { titleTreatmentLayout, stackedBrandingLockup } from './shared.js'
 
 export default function stackedPostMarkup() {
-  let T = View.endFrame;
+	let T = View.endFrame
 
-  // title treatment
-  titleTreatmentLayout(T);
+	// title treatment
+	titleTreatmentLayout(T)
 
-  Align.set(T.pedigree, {
-    x: {
-      type: Align.CENTER,
-      against: T.tt
-    },
-    y: {
-      type: Align.CENTER,
-      against: 65
-    }
-  });
+	Align.set(T.pedigree, {
+		x: {
+			type: Align.CENTER,
+			against: T.tt
+		},
+		y: {
+			type: Align.CENTER,
+			against: 65
+		}
+	})
 
-  stackedBrandingLockup(T, {
-    brandingLockupOffset: 8,
-    brandingLockupElemXAlign: Align.CENTER,
-    brandingLockupAlign: {
-      y: Align.CENTER,
-      x: {
-        type: Align.RIGHT,
-        offset: -12
-      }
-    },
-    headlineFontSize: 12
-  });
+	stackedBrandingLockup(T, {
+		brandingLockupOffset: 8,
+		brandingLockupElemXAlign: Align.CENTER,
+		brandingLockupAlign: {
+			y: Align.CENTER,
+			x: {
+				type: Align.RIGHT,
+				offset: -12
+			}
+		},
+		headlineFontSize: 12
+	})
 
-  // ratings bug
-  if (adData.hasRatings) {
-    Align.set(T.ratingsBug, {
-      x: {
-        type: Align.RIGHT,
-        offset: -5
-      },
-      y: {
-        type: Align.BOTTOM,
-        offset: -5
-      }
-    });
-  } else {
-    T.removeChild(T.ratingsBug);
-  }
+	// ratings bug
+	if (adData.hasRatings) {
+		Align.set(T.ratingsBug, {
+			x: {
+				type: Align.RIGHT,
+				offset: -5
+			},
+			y: {
+				type: Align.BOTTOM,
+				offset: -5
+			}
+		})
+	} else {
+		T.removeChild(T.ratingsBug)
+	}
 }
